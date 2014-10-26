7786bc306206abf57e4b575ebf863c73a5e68f17
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2014-10-27 00:45:59 +0200

    bubbles on homepage

---
+++ javascripts/home.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.circle-C-1').mouseover(function(){$('.bubble-C-1').css('display', 'initial')});
+  $('.circle-C-1').mouseout(function(){$('.bubble-C-1').removeAttr('style')});
+  
+  $('.circle-C-2').mouseover(function(){$('.bubble-C-2').css('display', 'initial')});
+  $('.circle-C-2').mouseout(function(){$('.bubble-C-2').removeAttr('style')});
+  
+  $('.circle-C-3').mouseover(function(){$('.bubble-C-3').css('display', 'initial')});
+  $('.circle-C-3').mouseout(function(){$('.bubble-C-3').removeAttr('style')});
+  
+  $('.circle-C-4').mouseover(function(){$('.bubble-C-4').css('display', 'initial')});
+  $('.circle-C-4').mouseout(function(){$('.bubble-C-4').removeAttr('style')});
+  
+  $('.circle-C-5').mouseover(function(){$('.bubble-C-5').css('display', 'initial')});
+  $('.circle-C-5').mouseout(function(){$('.bubble-C-5').removeAttr('style')});
+  
+  $('.circle-S-1').mouseover(function(){$('.bubble-S-1').css('display', 'initial')});
+  $('.circle-S-1').mouseout(function(){$('.bubble-S-1').removeAttr('style')});
+  
+  $('.circle-S-2').mouseover(function(){$('.bubble-S-2').css('display', 'initial')});
+  $('.circle-S-2').mouseout(function(){$('.bubble-S-2').removeAttr('style')});
+  
+  $('.circle-S-3').mouseover(function(){$('.bubble-S-3').css('display', 'initial')});
+  $('.circle-S-3').mouseout(function(){$('.bubble-S-3').removeAttr('style')});
+  
+  $('.circle-S-4').mouseover(function(){$('.bubble-S-4').css('display', 'initial')});
+  $('.circle-S-4').mouseout(function(){$('.bubble-S-4').removeAttr('style')});
+  
+  $('.circle-B-1').mouseover(function(){$('.bubble-B-1').css('display', 'initial')});
+  $('.circle-B-1').mouseout(function(){$('.bubble-B-1').removeAttr('style')});
+  
+  $('.circle-B-2').mouseover(function(){$('.bubble-B-2').css('display', 'initial')});
+  $('.circle-B-2').mouseout(function(){$('.bubble-B-2').removeAttr('style')});
+  
+  $('.circle-B-3').mouseover(function(){$('.bubble-B-3').css('display', 'initial')});
+  $('.circle-B-3').mouseout(function(){$('.bubble-B-3').removeAttr('style')});
+})
